21f22e45ab7e71e46a8c99f484367c480d806b01
Ilija Vucetic <vuceticilija@gmail.com>
2017-01-17 22:50:08 +0100

    Finish weather app :) .

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map'
 
 class WeatherList extends Component {
 
@@ -10,10 +11,13 @@ class WeatherList extends Component {
         const temps = cityData.list.map(weather => weather.main.temp);
         const pressures = cityData.list.map(weather => weather.main.pressure);
         const humidities = cityData.list.map(weather => weather.main.humidity);
+        const lon = cityData.city.coord.lon;
+        const lat = cityData.city.coord.lat;
+
 
         return (
             <tr key={name}>
-                <td>{name}</td>
+                <td><GoogleMap lon={lon} lat={lat}/></td>
                 <td><Chart data={temps} color="orange" units="K"/></td>
                 <td><Chart data={pressures} color="green" units="hPa"/></td>
                 <td><Chart data={humidities} color="black" units="%"/></td>
@@ -34,7 +38,6 @@ class WeatherList extends Component {
                         <th>Humidity(%)</th>
                     </tr>
                     </thead>
-
                     <tbody>
                     {this.props.weather.map(this.renderWeather)}
                     </tbody>
